f71014cb295cf2a6391c257fdb561393f083b31c
ashoksrinivasan96 <ashoksrinivasan96@gmail.com>
2020-09-25 00:00:34 +0530

    Added Axios github api call

--- githubprofilefetcher/src/App.js
+++ githubprofilefetcher/src/App.js
@@ -5,16 +5,17 @@ import CardList from './components/CardListComponent';
 import Card from './components/CardComponent';
 import Form from './components/FormComponent';
 
-const testData = [
-  {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
-  {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
-  {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
-];
+
 
 class App extends React.Component{ 
  
   state = {
-    profiles: testData,
+    profiles: []
+  };
+  addNewProfile = (profileData) => {
+  	this.setState(prevState => ({
+    	profiles: [...prevState.profiles, profileData],
+    }));
   };
   render() {
   return (
@@ -22,7 +23,7 @@ class App extends React.Component{
       <header className="App-header">
       <div>
     	  <div className="header">Github Profile Card App</div>
-        <Form />
+        <Form onSubmit={this.addNewProfile} />
         <CardList profiles={this.state.profiles} />
     	</div>
       </header>
--- githubprofilefetcher/src/components/CardListComponent.js
+++ githubprofilefetcher/src/components/CardListComponent.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const CardList = (props) => (
 	<div>
-  	{props.profiles.map(profile => <Card {...profile}/>)}
+  	{props.profiles.map(profile => <Card key={profile.id} {...profile}/>)}
 	</div>
 );
 
